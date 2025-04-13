5bd776024285f96a5f6dae4f5099b9098c580bf5
Prythomn <107319458+Prythomn@users.noreply.github.com>
2025-04-13 21:12:08 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -13,7 +13,7 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/Prythomn/android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Prythomn/android_device_xiaomi_munch.git -b 15-qpr1 device/xiaomi/munch; \
 
 git clone https://github.com/munch-devs/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 

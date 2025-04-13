9d32586d1e962b276b08d60c056a2b755c1ede27
Prythomn <107319458+Prythomn@users.noreply.github.com>
2025-04-13 14:30:24 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -3,8 +3,8 @@
 rm -rf .repo/local_manifests; \
 
 #repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
-repo init --depth=1 https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
-#repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
+#repo init --depth=1 https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
+repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs; \
 
 /opt/crave/resync.sh; \
@@ -18,7 +18,7 @@ git clone https://github.com/olzhas0986-dev/device_xiaomi_sm8250-common device/x
 git clone https://github.com/rik-x777/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
@@ -29,8 +29,6 @@ git clone https://codeberg.org/rik/keys.git vendor/lineage-priv/keys; \
 #git clone https://codeberg.org/rik/keys.git -b inf vendor/infinity-priv/keys; \
 
 . build/envsetup.sh; \
-breakfast munch
-croot
 brunch munch
 #lunch infinity_munch-userdebug && mka bacon; \
 #riseup munch userdebug && rise b

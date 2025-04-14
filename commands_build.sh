4ba5bd48ece45c8604bb0883583f750a6c7c6dc4
Prythomn <107319458+Prythomn@users.noreply.github.com>
2025-04-14 19:18:36 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -3,10 +3,11 @@
 rm -rf .repo/local_manifests; \
 
 #repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
-#repo init --depth=1 https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
+#repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
 #repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs; \
-repo init --depth=1 -u https://github.com/Evolution-X/manifest -b vic --git-lfs; \
+#repo init --depth=1 -u https://github.com/Evolution-X/manifest -b vic --git-lfs; \
+repo init --depth=1 -u https://github.com/yaap/manifest.git -b fifteen --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -14,9 +15,9 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://codeberg.org/rik/device_xiaomi_munch.git device/xiaomi/munch; \
+git clone  device/xiaomi/munch; \
 
-git clone https://codeberg.org/rik/device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
+git clone  device/xiaomi/sm8250-common; \
 
 git clone https://github.com/rik-x777/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 

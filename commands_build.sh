ddee88127fe4dc7d4da4b93c0faef279f7e0f352
Prythomn <107319458+Prythomn@users.noreply.github.com>
2025-04-16 00:27:19 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -4,16 +4,16 @@ rm -rf .repo/local_manifests; \
 
 #repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/ProjectInfinity-X/manifest -b 15 --git-lfs; \
-#repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
+repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs; \
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+#repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://codeberg.org/rik/device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Prythomn/-RIK-device_xiaomi_munch.git device/xiaomi/munch; \
 git clone https://codeberg.org/rik/device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
 git clone https://github.com/Prythomn/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
@@ -30,8 +30,6 @@ git clone https://codeberg.org/rik/keys.git vendor/lineage-priv/keys; \
 #git clone https://codeberg.org/rik/keys.git -b inf vendor/infinity-priv/keys; \
 
 . build/envsetup.sh; \
-axion munch user; \
-axion munch gms core; \
-ax -br
+brunch munch
 #lunch infinity_munch-userdebug && mka bacon; \
 #riseup munch userdebug && rise b

e73113e81614658913590c9042d6cc56973b6011
Prythomn <107319458+Prythomn@users.noreply.github.com>
2025-04-15 23:24:48 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
+#repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/ProjectInfinity-X/manifest -b 15 --git-lfs; \
 #repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs; \
-#repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs; \
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -20,7 +20,7 @@ git clone https://github.com/Prythomn/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8
 
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 
-git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
+git clone https://github.com/munch-devs/android_hardware_xiaomi.git -b 15-qpr1 hardware/xiaomi; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 

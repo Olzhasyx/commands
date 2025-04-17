bd383ea79dc7d2113e12c098f0749c46f12c91e4
Prythomn <107319458+Prythomn@users.noreply.github.com>
2025-04-17 07:42:33 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -19,7 +19,7 @@ git clone https://github.com/Prythomn/-RIK-device_xiaomi_munch.git device/xiaomi
 git clone https://github.com/Prythomn/-RIK-device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
 git clone https://github.com/Prythomn/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
+git clone https://github.com/Prythomn/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 git clone https://codeberg.org/rik/packages_resources_devicesettings.git packages/resources/devicesettings; \

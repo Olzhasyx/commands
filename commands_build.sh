cd98a469338a2d6b29f6afa57d71aab70f4edee7
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-18 07:06:47 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -10,12 +10,20 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/Olzhasyx/-HD-android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://codeberg.org/rik/device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://codeberg.org/rik/device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
 git clone https://codeberg.org/rik/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
-git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
+git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
+git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi; \
+git clone https://codeberg.org/rik/vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
+git clone https://codeberg.org/rik/packages_resources_devicesettings.git packages/resources/devicesettings; \
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
+
+rm -rf vendor/lineage-priv/keys && rm -rf vendor/infinity-priv/keys; \
+
+git clone https://codeberg.org/rik/keys.git vendor/lineage-priv/keys; \
 
 . build/envsetup.sh; \
 

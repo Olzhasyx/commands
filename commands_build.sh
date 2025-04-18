2813383a6771d506017706442513f2862bbf0de8
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-18 20:30:04 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install -y ccache; \
+
 rm -rf .repo/local_manifests; \
 
 #repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
@@ -10,9 +12,8 @@ repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
-sudo apt -y update; \
-sudo apt -y upgrade; \
-sudo apt install -y ccache; \
+
+
 
 git clone https://codeberg.org/rik/device_xiaomi_munch.git device/xiaomi/munch; \
 git clone https://github.com/Olzhasyx/android_device_xiaomi_sm8250.git device/xiaomi/sm8250-common; \

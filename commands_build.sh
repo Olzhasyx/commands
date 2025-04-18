a0d0a41b1cf4199cbd94a71631768ed77757d2dc
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-18 18:22:36 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -2,18 +2,21 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+#repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
-rm -rf out/target/product/munch
+rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
+rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
+rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://codeberg.org/rik/device_xiaomi_munch.git device/xiaomi/munch; \
-git clone https://github.com/Olzhasyx/adxsm8250 device/xiaomi/sm8250-common; \
+git clone https://codeberg.org/rik/device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 git clone https://codeberg.org/rik/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://github.com/Olzhasyx/vxsm8250-common.git vendor/xiaomi/sm8250-common; \
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22. hardware/xiaomi; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 git clone https://codeberg.org/rik/packages_resources_devicesettings.git packages/resources/devicesettings; \
@@ -24,4 +27,5 @@ rm -rf vendor/lineage-priv/keys && rm -rf vendor/infinity-priv/keys; \
 git clone https://codeberg.org/rik/keys.git vendor/lineage-priv/keys; \
 
 . build/envsetup.sh; \
-lunch lineage_munch-bp1a-userdebug && m evolution
+brunch munch
+#lunch lineage_munch-bp1a-userdebug && m evolution

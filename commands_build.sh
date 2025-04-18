239d85e4e3ecabe561b8f0944c1dd59cd5f509f0
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-18 19:19:43 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -12,7 +12,7 @@ rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardw
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://codeberg.org/rik/device_xiaomi_munch.git device/xiaomi/munch; \
-git clone https://codeberg.org/rik/device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
+git clone https://github.com/Olzhasyx/android_device_xiaomi_sm8250.git device/xiaomi/sm8250-common; \
 git clone https://codeberg.org/rik/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \

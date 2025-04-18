dc25b088460206ec6447798eb0431aa1446b79c1
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-18 21:19:06 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -5,7 +5,8 @@ rm -rf .repo/local_manifests; \
 #repo init --depth=1 -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
 #repo init --depth=1 -u https://github.com/ProjectInfinity-X/manifest -b 15 --git-lfs; \
 #repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
-repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs; \
+#repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs; \
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
 
 /opt/crave/resync.sh; \
 
@@ -18,7 +19,7 @@ git clone https://codeberg.org/rik/device_xiaomi_sm8250-common.git device/xiaomi
 git clone https://codeberg.org/rik/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi; \
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 git clone https://codeberg.org/rik/packages_resources_devicesettings.git packages/resources/devicesettings; \
@@ -26,11 +27,11 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 
 rm -rf vendor/lineage-priv/keys && rm -rf vendor/infinity-priv/keys; \
 
-git clone https://codeberg.org/rik/keys.git vendor/lineage-priv/keys; \
+#git clone https://codeberg.org/rik/keys.git vendor/lineage-priv/keys; \
 #git clone https://codeberg.org/rik/keys.git -b inf vendor/infinity-priv/keys; \
 
 . build/envsetup.sh; \
-
+lunch aosp_munch-bp1a-user && mka bacon
 #brunch munch
 #lunch infinity_munch-userdebug && mka bacon; \
-riseup munch userdebug && rise b
+#riseup munch userdebug && rise b

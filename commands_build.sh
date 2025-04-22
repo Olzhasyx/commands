d96356bf9b4cd1e34d7596d7e74dcfc1d9455973
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-22 20:57:46 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -3,7 +3,8 @@
 rm -rf .repo/local_manifests; \
 
 #repo init --depth=1 -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+#repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -11,7 +12,7 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/Olzhasyx/-hdzung-android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Olzhasyx/-hdzung-android_device_xiaomi_munch.git -b vic-los device/xiaomi/munch; \
 
 git clone https://github.com/munch-devs/kernel_xiaomi_sm8250.git kernel/xiaomi/munch; \
 
@@ -24,10 +25,13 @@ git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.gi
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
+
 export BUILD_USERNAME=olzhas0986
 export BUILD_HOSTNAME=crave
-source build/envsetup.sh; \
+
 #lunch aosp_munch-bp1a-user; \
 #mka bacon
-lunch yaap_munch-bp1a && m yaap
+#lunch yaap_munch-bp1a && m yaap
+. build/envsetup.sh; \
+brunch munch
 

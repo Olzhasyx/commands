9dcccacf5e18cc022a746b61d084049ae285d1e4
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-23 06:30:28 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -5,7 +5,8 @@ rm -rf .repo/local_manifests; \
 #repo init --depth=1 -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
 #repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 #repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs; \
-repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs; \
+#repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs; \
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
 
 /opt/crave/resync.sh; \
 
@@ -13,28 +14,22 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/Olzhasyx/-hdzung-android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Olzhasyx/adrod_device_xiaomi_munch.git device/xiaomi/munch; \
 
 git clone https://github.com/munch-devs/kernel_xiaomi_sm8250.git kernel/xiaomi/munch; \
 
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
-
 export BUILD_USERNAME=olzhas0986
 export BUILD_HOSTNAME=crave
-export PIXELAGE_BUILD="munch"
 
 #lunch aosp_munch-bp1a-user; \
 #mka bacon
 #lunch yaap_munch-bp1a && m yaap
-. build/envsetup.sh; \
-lunch pixelage_munch-bp1a-user; \
-mka bacon
-
+. b*/e* && lunch infinity_munch-user && mka bacon

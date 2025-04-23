2b23463e6f6b3fff376a8eb5e1131415015ba6f6
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-23 07:11:25 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -6,7 +6,7 @@ rm -rf .repo/local_manifests; \
 #repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 #repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs; \
 #repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs; \
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
+repo init --depth=1 --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15; \
 
 /opt/crave/resync.sh; \
 
@@ -26,10 +26,7 @@ git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.gi
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 
-export BUILD_USERNAME=olzhas0986
-export BUILD_HOSTNAME=crave
-
 #lunch aosp_munch-bp1a-user; \
 #mka bacon
 #lunch yaap_munch-bp1a && m yaap
-. b*/e* && lunch infinity_munch-user && mka bacon
+. b*/e* && lunch infinity_munch-userdebug && mka bacon

88931da89bb46a58295a047f984e7a644295b176
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-25 01:38:35 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests; \
 
 #repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs; \
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -11,12 +11,11 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX && rm -rf kernel/xiaomi/munch && rm -rf kernel/xiaomi/sm8250; \
 
 git clone https://github.com/Olzhasyx/rik_munch device/xiaomi/munch; \
-git clone https://github.com/Olzhasyx/rik_sm8250-common device/xiaomi/sm8250-common; \
-git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250 -b backup kernel/xiaomi/sm8250; \
+git clone https://github.com/Olzhasyx/rik_sm8250-common -b new device/xiaomi/sm8250-common; \
+git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
-git clone https://codeberg.org/rik/vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \

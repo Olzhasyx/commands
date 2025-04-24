09bd83839f65c9cc701930f64a6659d6bdba5c03
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-24 06:08:27 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs; \
 snap install --classic ccache; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
-rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \ rm -rf kernel/xiaomi/munch && rm -rf kernel/xiaomi/sm8250; \
+rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX && rm -rf kernel/xiaomi/munch && rm -rf kernel/xiaomi/sm8250; \
 
 git clone https://github.com/Olzhasyx/rik_munch device/xiaomi/munch; \
 git clone https://github.com/Olzhasyx/rik_sm8250-common device/xiaomi/sm8250-common; \

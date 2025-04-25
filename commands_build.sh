8d7fe2102748b08e6182bada2bdb97a3d9ce6d1b
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-04-25 17:41:05 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -2,29 +2,31 @@
 
 rm -rf .repo/local_manifests; \
 
-#repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs; \
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
+repo init -u https://github.com/RisingOS-Revived/android --git-lfs; \
+#repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX && rm -rf kernel/xiaomi/munch && rm -rf kernel/xiaomi/sm8250; \
 
-git clone https://github.com/Olzhasyx/device_xiaomi_munch device/xiaomi/munch; \
-git clone https://github.com/Olzhasyx/device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
+git clone https://github.com/Olzhasyx/android_device_xiaomi_munch-HDZungx -b 15-qpr1 device/xiaomi/munch; \
+
 git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250 kernel/xiaomi/sm8250; \
-git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git -b 15-qpr1 vendor/xiaomi/munch; \
+
+git clone https://github.com/munch-devs/android_hardware_xiaomi.git -b 15-qpr1 hardware/xiaomi; \
+
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
+
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
 
 export BUILD_USERNAME=olzhas0986
 
 . build/envsetup.sh; \
-breakfast munch; \
-croot; \
-brunch munch
-#riseup munch userdebug; \
-#rise b
+#breakfast munch; \
+#croot; \
+#brunch munch
+riseup munch userdebug; \
+rise b

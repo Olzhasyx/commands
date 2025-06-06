f529585e3c765e16368de5e008013bdcd440af4b
olzhas0986 <138654744+Olzhasyx@users.noreply.github.com>
2025-06-06 09:37:10 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -8,12 +8,12 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone  device/xiaomi/munch; \
-git clone  device/xiaomi/sm8250-common; \
+git clone https://github.com/Olzhasyx/device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Olzhasyx/device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://github.com/Olzhasyx/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
-git clone  kernel/xiaomi/sm8250; \
+git clone https://github.com/Olzhasyx/kernel_xiaomi_sm8250-hd.git -b ksu-susfs kernel/xiaomi/sm8250; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \

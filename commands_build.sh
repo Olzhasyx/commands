8b55e2a4231ffb9dee6282b2399e11cc14d6e7ad
olzhas0986 <138654744+olzhas0986dev@users.noreply.github.com>
2025-06-07 22:06:43 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -8,12 +8,13 @@ rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/x
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/munch-devs/android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/olzhas0986dev/android_device_xiaomi_munch device/xiaomi/munch; \
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://github.com/olzhas0986dev/kernel_xiaomi_sm8250-hd.git -b ksu-susfs kernel/xiaomi/munch; \
 git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://github.com/olzhas0986dev/vendor_gms.git vendor/gms; \
 rm -rf vendor/lineage-priv/keys && rm -rf vendor/infinity-priv/keys; \
 
 cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \

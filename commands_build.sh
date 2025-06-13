c9492a6d6fab8cc0e580aab08e955808b5590a95
olzhas0986 <138654744+olzhas0986dev@users.noreply.github.com>
2025-06-13 21:13:43 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -20,6 +20,7 @@ git clone https://github.com/olzhas0986dev/kernel_xiaomi_sm8250.git kernel/xiaom
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 rm -rf vendor/lineage-priv/keys && rm -rf vendor/infinity-priv/keys; \
 
 cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
